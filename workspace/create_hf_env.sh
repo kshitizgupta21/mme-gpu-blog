7a6ac2ab3a20610cf0bab6a230f57c21af992e40
EC2 Default User <ec2-user@ip-172-16-80-167.us-west-2.compute.internal>
2022-10-23 08:03:05 +0000

    finish add t5 and distilbert code

--- workspace/create_hf_env.sh
+++ workspace/create_hf_env.sh
@@ -4,6 +4,7 @@ conda create -y -n hf_env python=3.8
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate hf_env
 export PYTHONNOUSERSITE=True
-pip install torch transformers
+pip install torch --extra-index-url https://download.pytorch.org/whl/cu116
+pip install transformers[sentencepiece]
 pip install conda-pack
 conda-pack
