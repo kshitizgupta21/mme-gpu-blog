597cbf005ddffad4863b5db75f9cf010400d1b3b
EC2 Default User <ec2-user@ip-172-16-85-219.us-west-2.compute.internal>
2022-10-23 17:20:25 +0000

    cleanup

--- workspace/.ipynb_checkpoints/generate_distilbert_tf-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# use this script to export DistilBERT Classifcation TF model as a savedmodel file
-
-echo "Installing Transformers..."
-pip -q install transformers
-
-python tf_exporter.py
