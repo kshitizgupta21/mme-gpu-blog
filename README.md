1491985ac0c534e7ec93f143e187d8056e82b0ca
EC2 Default User <ec2-user@ip-172-16-93-201.us-west-2.compute.internal>
2022-11-17 00:39:22 +0000

    reduce t5 input len

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Serve Multiple DL models on GPU with Amazon SageMaker Multi-model endpoints (MME)
 
-In this example, we will walk you through how to use NVIDIA Triton Inference Server on Amazon SageMaker MME with GPU feature to deploy two different HuggingFace NLP transformer models (**DistilBERT** and **T5**) for two different use-cases (**Text Classification** and **Text Summarization**) in two different frameworks (**TensorFlow** and **PyTorch**) on the same GPU. In particular, these are the models we will be using:
+In this example, we will walk you through how to use NVIDIA Triton Inference Server on Amazon SageMaker MME with GPU feature to deploy two different HuggingFace NLP transformer models (**DistilBERT** and **T5**) for two different use-cases (**Text Classification** and **Text Translation**) in two different frameworks (**TensorFlow** and **PyTorch**) on the same GPU. In particular, these are the models we will be using:
 
 1. DistilBERT HuggingFace Classification TensorFlow Model (Served using Triton's TensorFlow Backend)
-2. T5-small HuggingFace PyTorch Summarization Model (Served using Triton's Python Backend) 
+2. T5-small HuggingFace PyTorch Translation Model (Served using Triton's Python Backend) 
 
 ## Steps to run the notebook
 
@@ -11,4 +11,4 @@ In this example, we will walk you through how to use NVIDIA Triton Inference Ser
     * IMPORTANT: In Notebook instance settings, within Additional Configuration, for **Volume Size in GB** specify at least **100 GB**.
     * For git repositories select the option `Clone a public git repository to this notebook instance only` and specify the Git repository URL https://github.com/kshitizgupta21/mme-gpu-blog
     
-2. Once JupyterLab is ready, launch the **mme-gpu.ipynb** notebook with **conda_python3** conda kernel and run through this notebook to learn how to host multiple NLP models on `g5.xlarge` GPU behind MME endpoint.
+2. Once JupyterLab is ready, launch the **mme-gpu.ipynb** notebook with **conda_python3** conda kernel and run through this notebook to learn how to host multiple NLP models on `g5.2xlarge` GPU behind MME endpoint.
--- workspace/model.py
+++ workspace/model.py
@@ -11,6 +11,20 @@ import triton_python_backend_utils as pb_utils
 class TritonPythonModel:
     # Every Python model must have "TritonPythonModel" as the class name!
     def initialize(self, args):
+        """`initialize` is called only once when the model is being loaded.
+        Implementing `initialize` function is optional. This function allows
+        the model to initialize any state associated with this model.
+        Parameters
+        ----------
+        args : dict
+          Both keys and values are strings. The dictionary keys and values are:
+          * model_config: A JSON string containing the model configuration
+          * model_instance_kind: A string containing model instance kind
+          * model_instance_device_id: A string containing model instance device ID
+          * model_repository: Model repository path
+          * model_version: Model version
+          * model_name: Model name
+        """
         self.output_dtype = pb_utils.triton_string_to_numpy(
             pb_utils.get_output_config_by_name(
                 json.loads(args['model_config']), "output"
@@ -22,6 +36,20 @@ class TritonPythonModel:
         print("TritonPythonModel initialized")
 
     def execute(self, requests):
+        """`execute` must be implemented in every Python model. `execute`
+        function receives a list of pb_utils.InferenceRequest as the only
+        argument. This function is called when an inference is requested
+        for this model.
+        Parameters
+        ----------
+        requests : list
+          A list of pb_utils.InferenceRequest
+        Returns
+        -------
+        list
+          A list of pb_utils.InferenceResponse. The length of this list must
+          be the same as `requests`
+        """
         responses = []
         for request in requests:
             input_ids = pb_utils.get_input_tensor_by_name(request, "input_ids")
@@ -31,14 +59,14 @@ class TritonPythonModel:
             attention_mask = attention_mask.as_numpy()
             attention_mask = torch.as_tensor(attention_mask).long().cuda()
             inputs = {'input_ids': input_ids, 'attention_mask': attention_mask}
-            summary = self.model.generate(**inputs, max_length=128, num_beams=1)
+            translation = self.model.generate(**inputs, num_beams=1)
             # Convert to numpy array on cpu:
-            np_summary = summary.cpu().int().detach().numpy()
+            np_translation =  translation.cpu().int().detach().numpy()
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[
                     pb_utils.Tensor(
                         "output",
-                        np_summary.astype(self.output_dtype)
+                        np_translation.astype(self.output_dtype)
                     )
                 ]
             )
@@ -46,4 +74,8 @@ class TritonPythonModel:
         return responses
 
     def finalize(self):
-        print("TritonPythonModel finalized")
+        """`finalize` is called only once when the model is being unloaded.
+        Implementing `finalize` function is optional. This function allows
+        the model to perform any necessary clean ups before exit.
+        """
+        print('Cleaning up...')
